3c62d21699cecdba4e7f3e20b6597b49fd2cc172
Icaro Almeida <progalmeidaregis@outlook.com>
2026-09-01 20:03:46 -0300

    style(AuthHeader): simplify keyboard visibility check

--- src/components/AuthHeader/index.tsx
+++ src/components/AuthHeader/index.tsx
@@ -4,9 +4,7 @@ import { Image, View } from "react-native";
 export const AuthHeader = () => {
   const keyboardVisible = useKeyboardVisible();
 
-  if (keyboardVisible) {
-    return null;
-  }
+  if (keyboardVisible) return null;
 
   return (
     <View className="flex-row items-center justify-center min-h-40 w-full gap-4">
