545fb4a7996db60ef87ee6983a36513eba9f9a6e
Icaro Almeida <progalmeidaregis@outlook.com>
2026-09-02 22:42:18 -0300

    feat(app): wrap app root with AuthContextProvider

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import Routes from '@/routes';
 import './src/styles/global.css';
+import { AuthContextProvider } from '@/context/auth.context';
 
 export default function App() {
   return (
-    <Routes />
+    <AuthContextProvider>
+      <Routes />
+    </AuthContextProvider>
   );
 }
