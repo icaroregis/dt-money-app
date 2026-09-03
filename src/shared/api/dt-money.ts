653827842847c1f3b55b9632fab3698712da0a46
Icaro Almeida <progalmeidaregis@outlook.com>
2026-09-02 23:59:01 -0300

    chore(api): configure local dev api base url and env file

--- src/shared/api/dt-money.ts
+++ src/shared/api/dt-money.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import { Platform } from 'react-native';
+// import { Platform } from 'react-native';
 
 // Se estiver usando um DISPOSITIVO FÍSICO, substitua 'SEU_IP_AQUI' pelo IP local do seu computador na rede Wi-Fi.
 // Exemplo: 'http://192.168.1.15:3001'
 // Para achar seu IP no Linux, rode o comando `ip a` ou `hostname -I` no terminal.
 
-const baseURL = 'http://SEU_IP_AQUI:3001';
+const baseURL = 'http://192.168.18.18:3001';
 
 // Caso volte a usar emuladores, você pode descomentar o código abaixo:
 /*
