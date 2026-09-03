fc34201e8aaec30c541baebe5f770f6670f0a58b
Icaro Almeida <progalmeidaregis@outlook.com>
2026-09-02 23:22:00 -0300

    feat(auth): implement auth logic and proper typing

--- src/context/auth.context.tsx
+++ src/context/auth.context.tsx
@@ -1,9 +1,11 @@
+import { IUser } from "@/shared/interfaces/user-interface";
 import { LoginFormValues } from "@/screens/Login/LoginForm";
 import { RegisterFormValues } from "@/screens/Register/RegisterForm";
+import * as AuthService from "@/shared/services/dt-money/auth.service";
 import { createContext, FC, PropsWithChildren, useState, useCallback, useMemo, useContext } from "react";
 
 type AuthContextType = {
-  user: null;
+  user: IUser | null;
   token: string | null;
   handleAuthenticate: (params: LoginFormValues) => Promise<void>;
   handleRegister: (params: RegisterFormValues) => Promise<void>;
@@ -13,10 +15,15 @@ type AuthContextType = {
 export const AuthContext = createContext<AuthContextType>({} as AuthContextType);
 
 export const AuthContextProvider: FC<PropsWithChildren> = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [token, setToken] = useState(null);
+  const [user, setUser] = useState<IUser | null>(null);
+  const [token, setToken] = useState<string | null>(null);
 
-  const handleAuthenticate = useCallback(async (params: LoginFormValues) => { }, []);
+  const handleAuthenticate = useCallback(async (params: LoginFormValues) => {
+    const { user, token } = await AuthService.authenticate(params);
+    setUser(user);
+    setToken(token);
+  }, []);
+
   const handleRegister = useCallback(async (params: RegisterFormValues) => { }, []);
   const handleLogout = useCallback(() => { }, []);
 
