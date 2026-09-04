89ef82f8a38bc9350f3680c53178313cee96755c
Icaro Almeida <progalmeidaregis@outlook.com>
2026-09-03 23:14:48 -0300

    feat(auth-context): add auth session persistence

--- src/context/auth.context.tsx
+++ src/context/auth.context.tsx
@@ -1,7 +1,9 @@
 import { IUser } from "@/shared/interfaces/user-interface";
 import { LoginFormValues } from "@/screens/Login/LoginForm";
 import { RegisterFormValues } from "@/screens/Register/RegisterForm";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as AuthService from "@/shared/services/dt-money/auth.service";
+import { IAuthenticateResponse } from "@/shared/interfaces/https/authenticate-response";
 import { createContext, FC, PropsWithChildren, useState, useCallback, useMemo, useContext } from "react";
 
 type AuthContextType = {
@@ -10,6 +12,7 @@ type AuthContextType = {
   handleAuthenticate: (params: LoginFormValues) => Promise<void>;
   handleRegister: (params: RegisterFormValues) => Promise<void>;
   handleLogout: () => void;
+  restoreUserSession: () => Promise<string | null>;
 }
 
 export const AuthContext = createContext<AuthContextType>({} as AuthContextType);
@@ -20,25 +23,38 @@ export const AuthContextProvider: FC<PropsWithChildren> = ({ children }) => {
 
   const handleAuthenticate = useCallback(async (params: LoginFormValues) => {
     const { user, token } = await AuthService.authenticate(params);
+    await AsyncStorage.setItem("dt-money-data", JSON.stringify({ user, token }));
     setUser(user);
     setToken(token);
   }, []);
 
   const handleRegister = useCallback(async (params: RegisterFormValues) => {
     const { user, token } = await AuthService.registerUser(params);
+    await AsyncStorage.setItem("dt-money-data", JSON.stringify({ user, token }));
     setUser(user);
     setToken(token);
   }, []);
 
   const handleLogout = useCallback(() => { }, []);
 
+  const restoreUserSession = useCallback(async () => {
+    const data = await AsyncStorage.getItem("dt-money-data");
+    if (data) {
+      const { user, token } = JSON.parse(data) as IAuthenticateResponse;
+      setUser(user);
+      setToken(token);
+    }
+    return data;
+  }, []);
+
   const value = useMemo(() => ({
     user,
     token,
     handleAuthenticate,
     handleRegister,
-    handleLogout
-  }), [user, token, handleAuthenticate, handleRegister, handleLogout]);
+    handleLogout,
+    restoreUserSession
+  }), [user, token, handleAuthenticate, handleRegister, handleLogout, restoreUserSession]);
 
   return (
     <AuthContext.Provider value={value}>
